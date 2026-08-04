6b5fed3d74f3c905011ba571f8cfcb30505961d5
Ayesha <ayesha.sa4e@gmail.com>
2026-08-04 16:09:20 +0530

    frontend url update

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://prescripto-ayeshasa.vercel.app/",
-      "https://prescripto-hqmm2a028-ayeshasa.vercel.app/",
+      "https://prescripto-ayeshasa.vercel.app",
+      "https://prescripto-hqmm2a028-ayeshasa.vercel.app",
       "http://localhost:5174",
     ],
     credentials: true,
